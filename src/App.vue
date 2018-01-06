ce1b96996295d310d00b5fc9d114b165a3f4da80
Orelia Sokambi <orelia.sokambi@gmail.com>
2018-01-06 13:29:09 +0100

    src/ : add assets folder + App.vue + main.js files

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Coucou tout le monde !</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
