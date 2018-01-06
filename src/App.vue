76524c456d519aaf0efeef12412e180d1ac4162e
Orelia Sokambi <orelia.sokambi@gmail.com>
2018-01-06 16:22:11 +0100

    App.vue : add AddBlog component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,29 @@
 <template>
   <div>
-    <h1>Coucou tout le monde !</h1>
+    <add-blog></add-blog>
   </div>
 </template>
 
 <script>
+import AddBlog from './components/AddBlog.vue';
+
 export default {
+  components:  {
+    'add-blog': AddBlog
+  },
   data () {
     return {
     }
+  },
+  methods:  {
+
   }
 }
 </script>
 
 <style>
-
+  * {
+    margin: 0;
+    font-family: 'Nunito', sans-serif;
+  }
 </style>
