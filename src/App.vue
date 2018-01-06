51e340cdd2caef308610aaa63ffc13dea824a6c3
Orelia Sokambi <orelia.sokambi@gmail.com>
2018-01-06 23:02:44 +0100

    add http request (post) wth json placeholder for test

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
-    <add-blog></add-blog>
+    <!--<add-blog></add-blog>-->
+    <show-blogs></show-blogs>
   </div>
 </template>
 
 <script>
-import AddBlog from './components/AddBlog.vue';
+import AddBlog from './components/AddBlog.vue'
+import ShowBlogs from './components/ShowBlogs.vue'
 
 export default {
   components:  {
-    'add-blog': AddBlog
+    'add-blog': AddBlog,
+    'show-blogs': ShowBlogs,
   },
   data () {
     return {
