bb7d715887660586a70ede266697981f15a1735b
Orelia Sokambi <orelia.sokambi@gmail.com>
2018-01-07 18:39:24 +0100

    App.vue: add export default component app-header

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <app-header></app-header>
     <router-view></router-view>
   </div>
 </template>
@@ -7,11 +8,13 @@
 <script>
 import AddBlog from './components/AddBlog.vue'
 import ShowBlogs from './components/ShowBlogs.vue'
+import Header from './components/Header.vue'
 
 export default {
   components:  {
     'add-blog': AddBlog,
     'show-blogs': ShowBlogs,
+    'app-header': Header,
   },
   data () {
     return {
