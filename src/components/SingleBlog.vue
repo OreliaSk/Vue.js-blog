a7727ccc849f99634d50ef703a79555c72917306
Orelia Sokambi <orelia.sokambi@gmail.com>
2018-01-07 19:36:25 +0100

    SingleBlog.vue : changed the $http request wth firebase

--- src/components/SingleBlog.vue
+++ src/components/SingleBlog.vue
@@ -1,7 +1,11 @@
 <template>
 	<div id="single-blog">
 		<h1>{{ blog.title }}</h1>
-		<article>{{ blog.body }}</article>
+		<article>{{ blog.content }}</article>
+		<p>Author : {{ blog.author }}</p>
+		<ul>
+			<li v-for="category in blog.categories">{{ category }}</li>
+		</ul>
 	</div>
 </template>
 
@@ -14,9 +18,11 @@
 			}
 		},
 		created(){
-			this.$http.get('https://jsonplaceholder.typicode.com/posts/' + this.id).then(function(data){
-				this.blog = data.body;
-			});
+			this.$http.get('https://vuejs-blog-711ae.firebaseio.com/posts/' + this.id + '.json').then(function(data){
+				return data.json();
+			}).then(function(data){
+				this.blog = data;
+			})
 		}
 	}
 </script>
