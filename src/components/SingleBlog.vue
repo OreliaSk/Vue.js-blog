8a7bef3ef08dff2f6f4740b17d3495020e54f484
Orelia Sokambi <orelia.sokambi@gmail.com>
2018-01-07 18:55:22 +0100

    SingleBlog.vue : add route parameters wth <router-link>

---
+++ src/components/SingleBlog.vue
@@ -0,0 +1,29 @@
+<template>
+	<div id="single-blog">
+		<h1>{{ blog.title }}</h1>
+		<article>{{ blog.body }}</article>
+	</div>
+</template>
+
+<script>
+	export default{
+		data(){
+			return{
+				id: this.$route.params.id,
+				blog: {},
+			}
+		},
+		created(){
+			this.$http.get('https://jsonplaceholder.typicode.com/posts/' + this.id).then(function(data){
+				this.blog = data.body;
+			});
+		}
+	}
+</script>
+
+<style>
+	#single-blog {
+		max-width: 960px;
+		margin: 0 auto;
+	}
+</style>
